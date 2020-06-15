e226fecba8e157a86571f3c35abe8ebcfbbd90af
Liyana <32006266+liyanalzm@users.noreply.github.com>
2020-06-15 21:28:42 +0800

    Add another section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="video-wrap">
-      <video muted loop autoPlay playsInline>
-        <source
-          src="https://www.liyanalzm.com/static/wind-24aec1ed6c350d2efc9227d74cecbc8c.mp4"
-          type="video/mp4"
-        />
-      </video>
-      <div className="text-overlay">hello</div>
-      <div className="background-overlay" />
+    <div>
+      <div className="video-wrap">
+        <video muted loop autoPlay playsInline>
+          <source
+            src="https://www.liyanalzm.com/static/wind-24aec1ed6c350d2efc9227d74cecbc8c.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <div className="text-overlay">hello</div>
+        <div className="background-overlay" />
+      </div>
+      <div style={{ height: "500px" }}>
+        This is just another section. Peace.
+      </div>
     </div>
   );
 }
